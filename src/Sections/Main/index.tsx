8584be0e362ae7315b15262618dc7869f8861568
LipeMachado <silvalipe765@gmail.com>
2022-09-22 17:08:05 -0300

    Add Projects

--- src/Sections/Main/index.tsx
+++ src/Sections/Main/index.tsx
@@ -7,6 +7,13 @@ import {
 import DefaultImage from '../../Assets/Images/powlandImage.jpeg'
 import HelloWorld from '../../Assets/Images/challenge-HelloWorld.jpeg'
 import AnimatedButton from '../../Assets/Images/challenge-AnimatedButton.jpeg'
+import HuddleLandingPage from '../../Assets/Images/challenge-HuddleLandingPage.jpeg'
+import LoginPage from '../../Assets/Images/challenge-LoginPage.jpeg'
+import NftCard from '../../Assets/Images/challenge-NFTCard.jpeg'
+import OrderSummary from '../../Assets/Images/challenge-OrderSummary.jpeg'
+import ProfileCard from '../../Assets/Images/challenge-ProfileCard.jpeg'
+import QrCode from '../../Assets/Images/challenge-QRCode.jpeg'
+import StatsPreview from '../../Assets/Images/challenge-StatsPreview.jpeg'
 
 export function Main() {
     return (
@@ -33,6 +40,55 @@ export function Main() {
                         Image={AnimatedButton}
                         UrlGitHub="https://github.com/LipeMachado/animatedButton-21DaysOfCode"
                         UrlDemo="https://animatedbutton-21daysofcode.vercel.app/"
+                    />
+                    <CardProjects
+                        Title="Huddle Landing Page"
+                        Description="Challenge performed from the FrontendMentor website."
+                        Image={HuddleLandingPage}
+                        UrlGitHub="https://github.com/LipeMachado/huddleLandingPage-Challenge"
+                        UrlDemo="https://huddlelandingpage-challenge.vercel.app/"
+                    />
+                    <CardProjects
+                        Title="Login Page"
+                        Description="Challenge performed from the FrontendMentor website."
+                        Image={LoginPage}
+                        UrlGitHub="https://github.com/LipeMachado/loginPage-Challenge"
+                        UrlDemo="https://loginpage-challenge.vercel.app/"
+                    />
+                    <CardProjects
+                        Title="NFT Card"
+                        Description="Challenge performed from the FrontendMentor website."
+                        Image={NftCard}
+                        UrlGitHub="https://github.com/LipeMachado/nftCard-Challenge"
+                        UrlDemo="https://nftcard-component-challenge.vercel.app/"
+                    />
+                    <CardProjects
+                        Title="Order Summary Card"
+                        Description="Challenge performed from the FrontendMentor website."
+                        Image={OrderSummary}
+                        UrlGitHub="https://github.com/LipeMachado/orderSummary-Challenge"
+                        UrlDemo="https://ordersummary-challenge.vercel.app/"
+                    />
+                    <CardProjects
+                        Title="Profile Card"
+                        Description="Challenge performed from the FrontendMentor website."
+                        Image={ProfileCard}
+                        UrlGitHub="https://github.com/LipeMachado/profileCard-Challenge"
+                        UrlDemo="https://profilecard-challenge.vercel.app/"
+                    />
+                    <CardProjects
+                        Title="QR Code Card"
+                        Description="Challenge performed from the FrontendMentor website."
+                        Image={QrCode}
+                        UrlGitHub="https://github.com/LipeMachado/qrCode-Challenge"
+                        UrlDemo="https://qrcode-challenge.vercel.app/"
+                    />
+                    <CardProjects
+                        Title="Stats Preview Card"
+                        Description="Challenge performed from the FrontendMentor website."
+                        Image={StatsPreview}
+                        UrlGitHub="https://github.com/LipeMachado/statsPreview-Challenge"
+                        UrlDemo="https://statspreview-challenge.vercel.app/"
                     />
                 </MainBox>
             </MainContainer>
